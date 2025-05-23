d86b8bdaa80772227f9b09c9c14e0989080dfd32
chris <christopher.neves@hotmail.ca>
2025-05-23 15:37:26 -0400

    project structure, package json files and navbar

---
+++ src/js/main.js
@@ -0,0 +1,16 @@
+
+
+
+
+// addItemContainer
+
+
+
+
+// Check local storage for existing projects, habits
+
+// Populate interface with all projects saved in user view options
+
+// Populate calendar
+
+// Populate git repo
